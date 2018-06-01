5c8b0ccc939e6a4b35ad01a8b68e4d6f3f64ca8e
vidman22 <jvidmar37@gmail.com>
2018-06-01 07:02:02 -0700

    svg

--- server/SocketManager.js
+++ server/SocketManager.js
@@ -3,7 +3,7 @@ const io = require('./index.js').io;
 // const uuidv1 = require('uuid/v1');
 // const uuidv3 = require('uuid/v3');
 
-const sessions = [];
+let sessions = [];
 
 class SessionObject {
 	constructor() {
@@ -18,23 +18,21 @@ class SessionObject {
 
 
 module.exports = function(socket) {
-
-	console.log("Socket ID " + socket.id);
 	
 
 	socket.on('NEW_ROOM', room => {
 		let newRoom = new SessionObject();
 		newRoom.room = room;
 		sessions.push(newRoom);
-		console.log(newRoom.room);
+		
 		socket.join(newRoom.room);
 		io.to(newRoom.room).emit('JOINED');
-		console.log('sessions ', sessions);
+		
 	});
 
 	socket.on('JOIN_ROOM', (room, callback) => {
 		socket.join(room);
-		// io.to(room).emit('JOINED');
+		
 		let temp_room = '';
 		let message = '';
 		for ( let i = 0; i < sessions.length; i++) {
@@ -54,7 +52,13 @@ module.exports = function(socket) {
 
 		const index = searchSessions( room );
 		const users = sessions[index].connectedUsers;
-		users.push(name);
+
+		const user = {
+			name: name,
+			id: socket.id,
+			score: 0
+		}
+		users.push(user);
 		
 		io.to(room).emit('UPDATED_PLAYERS', (room, users ));
 	});
@@ -63,18 +67,7 @@ module.exports = function(socket) {
 	socket.on('SHUFFLE', (room, users, cb) =>{
 		const index = searchSessions( room );
 
-		
-
-		let arrayOfTeams = [];
-		if ( users.length > 3 && users.length < 9 ) {
-			arrayOfTeams = createTwoTeams(users);
-		}
-		if ( users.length > 8 && users.length < 12 ) {
-			arrayOfTeams = createThreeTeams(users);
-		}
-		if ( users.length > 11 && users.length < 17 ) {
-			arrayOfTeams = createFourTeams(users);
-		}
+		const arrayOfTeams = createTeams(users);
 
 		sessions[index].teams = arrayOfTeams;
 
@@ -83,14 +76,51 @@ module.exports = function(socket) {
 
 	socket.on('START_GAME', (room, users, game) => {
 		io.to(room).emit('START_GAME', game);
-	})
+	});
 
-	socket.on('disconnect', () => {
-	
-		console.log('socket ' + socket.id + ' disconnected');
+	socket.on('SUCCESS',  (room, name)  => {
+		console.log("success for " + name);
+		const index = searchSessions(room);
+
+		let connectedUsers = sessions[index].connectedUsers;
+		console.log("connected users ln 86", connectedUsers);
+		for ( let i = 0; i < connectedUsers.length; i ++) {
+			if ( connectedUsers[i].name === name ) {
+				connectedUsers[i].score++;
+				io.to(room).emit('SCORE', connectedUsers);
+				console.log("connected users after success " , connectedUsers);
+			}
+		}
+		
 
 	});
 
+	socket.on('FAILURE', room => {
+
+	});
+
+	socket.on('disconnect', () => {
+	
+		for ( let i = 0; i < sessions.length; i++ ) {
+		  if (sessions[i].connectedUsers.length !== undefined) {	
+			for ( let j = 0; j < sessions[i].connectedUsers.length; j++){
+				if (sessions[i].connectedUsers[j].id === socket.id ) {
+					const id = sessions[i].connectedUsers[j].id; 
+
+					sessions[i].connectedUsers = sessions[i].connectedUsers.filter((user) => user.id !== id);
+
+					io.to(sessions[i].room).emit('USER_DISCONNECTED', sessions[i].connectedUsers[j]);
+
+					console.log('session ', sessions[i].connectedUsers, ' updated after disconnect');
+				}
+				if (sessions[i].connectedUsers.length === 0) {
+					sessions = sessions.filter((session) => sessions[i] );
+					console.log("new session after disconnect ", sessions );
+				}
+			}
+		  }
+		}
+	});
 }
 
 searchSessions = (room) => {
@@ -119,6 +149,28 @@ randomizeArray = (users) => {
 
 		} return array;
 
+}
+
+createTeams = ( users ) => {
+	console.log("create teams triggered");
+	const numberOfTeams = Math.ceil(users.length/4); 
+	const array = randomizeArray(users);
+	
+	const firstSegment = Math.floor(array.length/numberOfTeams);
+	let firstBreak = 0;
+	let newBreak = Math.floor(array.length/numberOfTeams);
+
+	let teams = [];
+	console.log("number of teams " + numberOfTeams );
+	for ( let i = 0; i < numberOfTeams; i++) {
+		const team = array.slice(firstBreak, newBreak);
+		newBreak = newBreak + firstSegment;
+		firstBreak = firstBreak + firstSegment;
+		teams.push(team);
+		console.log("team ", team);
+	}
+	return teams;
+	console.log("create teams ", teams);
 }
 
 createTwoTeams = ( users ) => {
