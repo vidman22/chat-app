6c18a3c1c3a4a9817d5076c9bd788eb5a0c22491
vidman22 <jvidmar37@gmail.com>
2018-06-13 21:31:58 -0700

    server change

--- server/index.js
+++ server/index.js
@@ -15,8 +15,11 @@ io.on('connection', ( socket ) => {
 	SocketManager(socket);
 });
 
-app.use(express.static(__dirname + ' /../build'));
+// app.use(express.static(__dirname + ' /../build'));
 
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 server.listen(PORT, () => {
 	console.log("Connected on port " + port + "!");
